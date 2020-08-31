b24f48ba06ea3b96b66af854914db6fc3318e5c4
EliasLindvall <70134851+EliasLindvall@users.noreply.github.com>
2020-08-31 10:36:17 +0200

    skapat variabler, gjort inputs/outputs

---
+++ Variabler/variabler.py
@@ -0,0 +1,11 @@
+name = "Elias"
+age = 16
+print(f"Hej {name} du är {age} år gammal.")
+
+side = float(input("Ange kvadratens sida: "))
+
+area = side**2
+
+omkrets = 4*side 
+
+print(f"Kvadratens area är {area} a.e. och kvadratens omkrets är {omkrets} l.e")
