173794f6680da13df69a1b86893f03652346af01
EliasLindvall <70134851+EliasLindvall@users.noreply.github.com>
2020-10-21 21:47:53 +0200

    laboration

--- programmering/ok.py
+++ programmering/ok.py
@@ -3,7 +3,9 @@ Uppgift 1
 
 a)
 '''
-
+print("   ")
+print("   a)")
+print("   ")
 import math 
 
 x1=0
@@ -21,6 +23,10 @@ print (f"distansen mellan punkterna är {dist}")
 '''
 b)
 '''
+print("   ")
+print("   b)")
+print("   ")
+
 import math 
 
 x1=0
@@ -38,6 +44,10 @@ print (f"distansen mellan punkterna är {dist}")
 '''
 c)
 '''
+print("   ")
+print("   c)")
+print("   ")
+    
 import math 
 
 x1=0
@@ -55,7 +65,9 @@ print (f"distansen mellan punkterna är {dist}")
 '''
 d)
 '''
-
+print("   ")
+print("   d)")
+print("   ")
 import math 
 import random
 
@@ -74,67 +86,34 @@ print (x1, y1, x2, y2)
 '''
 e)
 '''
-import math 
+print("   ")
+print("   e)")
+print("   ")
+
+import math
 import random
 
 summa=0
 
-x1= 0
+for n in range (20):
+    
+    x1= random.uniform(-1,1)
+    y1= random.uniform(-1,1)
 
-y1= 0
+    dist= math.sqrt((x1-0)**2 + (y1-0)**2)
 
-x2= random.uniform(-1,1)
+    if dist <= 1:
+        summa +=1
 
-y2= random.uniform(-1,1)
 
-x3 = random.uniform(-1,1)
+print(summa)
 
-y3 = random.uniform(-1,1)
-
-x4 = random.uniform(-1,1)
-
-y4 = random.uniform(-1,1)
-
-dist1 = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-dist2 = math.sqrt((x4-x1)**2 + (y3-y1)**2)
-
-dist3 = math.sqrt((x4-x1)**2 + (y4-y1)**2)
-
-if dist1 < 1:
-    summa += 1
-
-if dist2 < 1:
-    summa += 1
-
-if dist3 < 1:
-    summa += 1
-
-divsum = summa/3
-
-print (f"procent av punkter som hamnade inom cirkeln {divsum*100}%")
 
 '''
 f)
 '''
-import math 
-import random
-
-x1= random.uniform(-1,1)
-
-y1= random.uniform(-1,1)
-
-x2= random.uniform(-1,1)
-
-y2= random.uniform(-1,1)
-
-
-
-print (x1, y1, x2, y2)
-
-'''
-e)
-'''
+print("   ")
+print("   ")
 import math 
 import random
 
@@ -174,6 +153,64 @@ if dist3 < 1:
 multsum = summa*4
 
 print (multsum)
+
+
+'''
+g)
+'''
+print("   ")
+print("   ")
+
+import matplotlib as plt
+import math 
+import random
+
+summa=0
+
+x1= 0
+
+y1= 0
+
+x2= random.uniform(-1,1)
+
+y2= random.uniform(-1,1)
+
+x3 = random.uniform(-1,1)
+
+y3 = random.uniform(-1,1)
+
+x4 = random.uniform(-1,1)
+
+y4 = random.uniform(-1,1)
+
+x5 = random.uniform(-1,1)
+
+y5 = random.uniform(-1,1)
+
+dist1 = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+dist2 = math.sqrt((x4-x1)**2 + (y3-y1)**2)
+
+dist3 = math.sqrt((x4-x1)**2 + (y4-y1)**2)
+
+dist4 = math.sqrt((x5-x1)**2 + (y5-y1)**2)
+
+if dist1 < 1:
+    summa += 1
+
+if dist2 < 1:
+    summa += 1
+
+if dist3 < 1:
+    summa += 1
+
+if dist4 < 1:
+    summa += 1
+
+plt.plot (x2, y2, '*r')
+plt.plot (x3, y3, '*g')
+plt.show
+print (summa)
 
 
 
