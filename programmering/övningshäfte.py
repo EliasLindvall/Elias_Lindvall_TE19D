c7716ed4294ba7fe0ed67bb2685eb53012930ac4
EliasLindvall <70134851+EliasLindvall@users.noreply.github.com>
2020-12-07 13:03:31 +0100

    p

--- programmering/övningshäfte.py
+++ programmering/övningshäfte.py
@@ -1,4 +1,41 @@
+'''
 def aritmetisk_summa(n,a1,an):
     return (n*(a1+an))/2
 
 print (aritmetisk_summa(100,1,100))
+
+
+b)
+
+def aritmetisk_summa(n,a1,an):
+    return (n*(a1+an))/2
+
+print (aritmetisk_summa(100,1,100))
+
+
+
+
+c)
+
+
+
+def aritmetisk_summa(n,a1,an):
+    return (n*(a1+an))/2
+
+a1 = int(input("Skriv vilket nummer den aritmetiska talsumman ska börja på: "))
+
+an = int(input("Skriv vilket nummer den aritmetiska talsumman ska sluta på: "))
+
+n = int(input("Skriv hur många tal det är emellan första och sista talet i aritmetiska talsumman: "))
+
+print (aritmetisk_summa(100,1,100))
+
+'''
+'''
+2
+'''
+def aritmetisk_summa(n,a1,an):
+    return (2*n-1)
+
+print (aritmetisk_summa(100,1,100))
+
