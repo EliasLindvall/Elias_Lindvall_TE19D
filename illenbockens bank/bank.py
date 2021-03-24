d92110d7e075a00cc19ec447e9d5843f23e4a341
EliasLindvall <70134851+EliasLindvall@users.noreply.github.com>
2021-03-24 09:32:50 +0100

    bank meny

---
+++ illenbockens bank/bank.py
@@ -0,0 +1,17 @@
+while True:
+    meny = 
+    (f"\n############################"
+    "\n#                          #"
+    "\n#    illenbockens bank     #"
+    "\n#       Saldo: {}          #"
+    "\n#                          #"
+    "\n############################"
+    "\n "
+    "\n 1: Visa Saldo."
+    "\n 2: Sätt in pengar."
+    "\n 3: Ta ut pengar."
+    "\n 4: Avsluta applikationen.")
+
+    val = int()
+
+    
